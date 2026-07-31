e82cc6394c154faa769fc96ad4c2f03403a3049d
Tarun Chintakunta <tarunchintakunta@gmail.com>
2026-07-31 17:29:25 +0530

    feat(server): add repos layer

---
+++ server/repos/index.ts
@@ -0,0 +1,6 @@
+export * as clientsRepo from "./clients";
+export * as leadsRepo from "./leads";
+export * as dealsRepo from "./deals";
+export * as employeesRepo from "./employees";
+export * as payrollRepo from "./payroll";
+export * as attendanceRepo from "./attendance";
